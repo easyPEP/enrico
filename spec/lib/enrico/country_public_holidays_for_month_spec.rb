09440ff09c28bb49f92864586942f97b036983be
Mohammed Gharbi <mohammed@staffomatic.com>
2020-04-12 22:34:21 +0200

    feature: upgrade to enrico v2

--- spec/lib/enrico/country_public_holidays_for_month_spec.rb
+++ spec/lib/enrico/country_public_holidays_for_month_spec.rb
@@ -6,56 +6,56 @@ require_relative '../../spec_helper'
 
 describe Enrico::Country do
 
-  describe "GET getPublicHolidaysForMonth" do
+  describe "GET getHolidaysForMonth" do
     before(:each) do
-      VCR.insert_cassette 'public_holidays_in_month', :record => :new_episodes
+      VCR.insert_cassette 'holidays_in_month', :record => :new_episodes
     end
 
     after(:each) do
       VCR.eject_cassette
     end
-    let(:country) { Enrico::Country.new("ger") }
+    let(:country) { Enrico::Country.new("deu") }
     it "should return all holidays in month" do
       date                      = Date.today.beginning_of_year
-      dates                     = country.public_holidays_for_month(date)
+      dates                     = country.holidays_for_month(date)
       dates.first.date          = Date.today.beginning_of_year
       dates.first.local_name    = "Neujahrstag"
       dates.first.english_name  = "New Year's Day"
     end
   end
 
-  describe "GET getPublicHolidaysForYear" do
+  describe "GET getHolidaysForYear" do
     before(:each) do
-      VCR.insert_cassette 'public_holidays_in_year', :record => :new_episodes
+      VCR.insert_cassette 'holidays_in_year', :record => :new_episodes
     end
 
     after(:each) do
       VCR.eject_cassette
     end
-    let(:country) { Enrico::Country.new("ger") }
+    let(:country) { Enrico::Country.new("deu") }
     it "should return all holidays in month" do
       date                      = Date.today.beginning_of_year
-      dates                     = country.public_holidays_for_year(date)
+      dates                     = country.holidays_for_year(date)
       dates.first.date          = Date.today.beginning_of_year
       dates.first.local_name    = "Neujahrstag"
       dates.first.english_name  = "New Year's Day"
     end
   end
 
-  describe "GET getPublicHolidaysForDateRange" do
+  describe "GET getForDateRange" do
     before(:each) do
-      VCR.insert_cassette 'public_holidays_in_date_range', :record => :new_episodes
+      VCR.insert_cassette 'holidays_in_date_range', :record => :new_episodes
     end
 
     after(:each) do
       VCR.eject_cassette
     end
-    let(:country) { Enrico::Country.new("ger") }
+    let(:country) { Enrico::Country.new("deu") }
     it "should return all holidays in date range" do
       start_date = Date.today.beginning_of_year
       end_date = Date.today.beginning_of_year + 2.month
 
-      dates = country.public_holidays_for_date_range(start_date, end_date)
+      dates = country.holidays_for_date_range(start_date, end_date)
 
       dates.first.date          = Date.today.beginning_of_year
       dates.first.local_name    = "Neujahrstag"
@@ -64,4 +64,3 @@ describe Enrico::Country do
   end
 
 end
-
